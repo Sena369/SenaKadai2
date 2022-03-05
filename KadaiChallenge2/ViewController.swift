8eebc4ea3fb2fc983b50a2e354ef6b6381f413f7
Sena <senana0116@icloud.com>
2022-03-05 10:22:16 +0900

    ファイル作成

---
+++ KadaiChallenge2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KadaiChallenge2
+//
+//  Created by 澤田世那 on 2022/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
