345a663d407cc78d6a618aed3de6d9f45fef76dc
Sena <senana0116@icloud.com>
2022-03-05 21:59:54 +0900

    条件式の書き出し

--- KadaiChallenge2/ViewController.swift
+++ KadaiChallenge2/ViewController.swift
@@ -9,25 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
 
-    @IBOutlet private weak var firrstText: UITextField!
+    @IBOutlet private weak var firstText: UITextField!
     @IBOutlet private weak var secondText: UITextField!
     
-    @IBOutlet private weak var calculationSegmented: UISegmentedControl!
+    @IBOutlet weak var calculationSegmented: UISegmentedControl!
     
+    @IBOutlet weak var resultLabel: UILabel!
     
+    @IBAction func changeSelect(_ sender: Any) {
+        
+//        let num = calculationSegmented.selectedSegmentIndex
     
-    
-    @IBAction func calculationSegmented(_ sender: Any) {
+        if calculationSegmented.selectedSegmentIndex == 0 {
+        } else if num == 1 {
+        } else if num == 2 {
+        } else if num == 3 {
     }
     
-    @IBAction func calculateButton(_ sender: Any) {
+  
+    func calculationButton() {
+        let num = calculationSegmented.selectedSegmentIndex
+        
+        let firstValue = Int(firstText.text ?? "") ?? 0
+        let secondValue = Int(secondText.text ?? "") ?? 0
+        
+        let resultValue = valueCalculate(value1: firstValue, value2: secondValue)
+    
+        
     }
     
-    @IBOutlet private weak var resultLabel: UILabel!
+        func additionCalculate (additionValue1: Int , additionValue2: Int) -> Int {
+                return additionValue1 + additionValue2
+        }
+        
+        func subtractionCalculate (subtractionvalue1: Int , subtractionvalue2: Int) -> Int {
+            return subtractionvalue1 - subtractionvalue2
+        }
+  
+        func multiplicationCalculate (multiplicationvalue1: Int , multiplicationvalue2: Int) -> Int {
+                return multiplicationvalue1 * multiplicationvalue2
+        }
+        
+        func divisionCalculate (divisionvalue1: Int , divisionvalue2: Int) -> Int {
+                return divisionvalue1 / divisionvalue2
+        }
+    }
+    
+
+    
+
 }
 
